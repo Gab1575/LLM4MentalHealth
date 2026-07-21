7e800c1d377741eaafa4bebedd560adf767ea2e2
Gav <Gavin.Bayley@queensu.ca>
2026-07-21 12:01:30 -0400

    Merged ros2-opencv-localization into parent repo

---
+++ FullCode_ws/ros2-opencv-localization/launch/tracker.launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='usb_cam',
+            executable='usb_cam_node_exe',
+            name='usb_cam',
+            parameters=[
+                {'video_device': '/dev/video2'}, # The confirmed video capture node
+                {'image_width': 640},            # The hardware's preferred 30fps width
+                {'image_height': 480},           # The hardware's preferred 30fps height
+                {'framerate': 30.0}
+            ]
+        ),
+        
+        Node(
+            package='localization_tracker',
+            executable='localization_tracker_node',
+            name='localization_tracker_node'
+        )
+    ])
